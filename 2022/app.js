94d740ec48f0a392be748af39ba3eab55bdea298
jrolabarria <jrolabarria@leartik.eus>
2024-09-27 19:49:20 +0200

    22. bersiñoie

--- 2022/app.js
+++ 2022/app.js
@@ -1,58 +1,49 @@
-/*Variables*/
 
-const btnCierra = document.querySelector('#btn-cierra');
-const btnAdelanta = document.querySelector('#btn-adelanta');
-const btnRetrocede = document.querySelector('#btn-retrocede');
-const imagenes = document.querySelectorAll('#galeria img');
-const lightbox = document.querySelector('#contenedor-principal');
-const imagenActiva = document.querySelector('#img-activa');
-const texto = document.querySelector('#texto');
-let indiceImagen = 0;
+const botoiaItxi = document.querySelector('#botoia_itxi');
+const botoiaAurrekoa = document.querySelector('#botoia_aurrekoa');
+const botoiaHurrengoa = document.querySelector('#botoia_hurrengoa');
+const irudiak = document.querySelectorAll('#galeria img');
+const lightbox = document.querySelector('#lightbox');
+const irudia = document.querySelector('#irudia');
+const testua = document.querySelector('#testua');
+let irudiarenIndizea = 0;
 
-/*Abre el Lightbox*/
-
-const abreLightbox = (event) => {
-  imagenActiva.src = event.target.src;
-  imagenActiva.alt = event.target.alt;
+const lightboxaIreki = (event) => {
+  irudia.src = event.target.src;
+  irudia.alt = event.target.alt;
   lightbox.style.display = 'flex';
-  texto.innerText = imagenActiva.alt;
-  indiceImagen = Array.from(imagenes).indexOf(event.target);
+  testua.innerText = irudia.alt;
+  irudiarenIndizea = Array.from(irudiak).indexOf(event.target);
 };
 
-imagenes.forEach((imagen) => {
-  imagen.addEventListener('click', abreLightbox);
+irudiak.forEach((irudia) => {
+  irudia.addEventListener('click', lightboxaIreki);
 });
 
-/*Cierra el Lightbox */
-
-btnCierra.addEventListener('click', () => {
+botoiaItxi.addEventListener('click', () => {
   lightbox.style.display = 'none';
 });
 
-/* Adelanta la imagen*/
-
-const adelantaImagen = () => {
-  if (indiceImagen === imagenes.length - 1) {
-    indiceImagen = -1;
+const hurrengoaErakutsi = () => {
+  if (irudiarenIndizea === irudiak.length - 1) {
+    irudiarenIndizea = -1;
   }
-  imagenActiva.src = imagenes[indiceImagen + 1].src;
-  imagenActiva.alt = imagenes[indiceImagen + 1].alt;
-  texto.innerText = imagenActiva.alt;
-  indiceImagen++;
+  irudia.src = irudiak[irudiarenIndizea + 1].src;
+  irudia.alt = irudiak[irudiarenIndizea + 1].alt;
+  testua.innerText = irudia.alt;
+  irudiarenIndizea++;
 };
 
-btnAdelanta.addEventListener('click', adelantaImagen);
+botoiaHurrengoa.addEventListener('click', hurrengoaErakutsi);
 
-/*Retrocede la Imagen*/
-
-const retrocederImagen = () => {
-  if (indiceImagen === 0) {
-    indiceImagen = imagenes.length;
+const aurrekoaErakutsi = () => {
+  if (irudiarenIndizea === 0) {
+    irudiarenIndizea = irudiak.length;
   }
-  imagenActiva.src = imagenes[indiceImagen - 1].src;
-  imagenActiva.alt = imagenes[indiceImagen - 1].alt;
-  texto.innerText = imagenActiva.alt;
-  indiceImagen--;
+  irudia.src = irudiak[irudiarenIndizea - 1].src;
+  irudia.alt = irudiak[irudiarenIndizea - 1].alt;
+  testua.innerText = irudia.alt;
+  irudiarenIndizea--;
 };
 
-btnRetrocede.addEventListener('click', retrocederImagen);
+botoiaAurrekoa.addEventListener('click', aurrekoaErakutsi);
